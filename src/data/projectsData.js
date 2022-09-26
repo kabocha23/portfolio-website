74d19fbe204a948243e282b711e254092e3e7630
Jason Kobuchi <jekobuchi@gmail.com>
2022-09-26 16:51:27 -0700

    Added IP Finder to projects data list

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,14 +3,24 @@ const projectsData = [
         id: 0,
         title: 'Shortly',
         headline: 'A URL Shortening Application',
-        caption: 'React, Shrtcode API',
+        caption: 'React, Shrtcode API, Axios',
         image: 'shortly-preview',
-        description: 'A responsive web/mobile application that shortens the user\'s URL by tapping into the Shrtcode API. \nFeatures include: \n- input allowing user to make API call and shorten any url \n- dynamically generated search history list with copy to clipboard button \n- navbar transitioning to hidden menu (sliding in from top) \n- responsive design for web and mobile',
+        description: 'A responsive web application that shortens the user\'s URL by tapping into the Shrtcode API. \nFeatures include: \n- input allowing user to make API call and shorten any url \n- dynamically generated search history list with copy to clipboard button \n- navbar transitioning to hidden menu (sliding in from top) \n- responsive design for web and mobile',
         sourceCode: 'https://github.com/kabocha23/URL-Shortener',
         liveDemo: 'https://kabocha23.github.io/URL-Shortener'
     },
     {
         id: 1,
+        title: 'IP Finder',
+        headline: 'An app for curious people locating IP addresses, or weirdos...',
+        caption: 'React, Leaflet Maps API, Ipify API, Axios',
+        image: 'ip_finder_preview',
+        description: 'A responsive web application that allows the user to extract general information from a public ip address. \n- Input allowing user to search for the location, timezone, and ISP of any public IP Address \n(Leaving the input blank will search the user\'s IP Address) \n- Have the IP geolocation pinned on an interactive map \n- Responsive web/mobile design',
+        sourceCode: 'https://github.com/kabocha23/ip-address-tracker',
+        liveDemo: 'https://kabocha23.github.io/ip-address-tracker'
+    },
+    {
+        id: 2,
         title: 'Indecisive Eaters',
         headline: 'An app for anyone that cannot make up their mind',
         caption: 'React, Node, Express, Yelp-Fusion API, Axios, React DnD, reactjs-popup',
@@ -20,17 +30,17 @@ const projectsData = [
         liveDemo: 'https://whispering-forest-79782.herokuapp.com'
     },
     {
-        id: 2,
+        id: 3,
         title: 'Sneakers',
         headline: 'E-commerce Product Page',
         caption: 'React',
         image: 'sneakers2',
-        description: 'A responsive product page web and mobile design for a fictional e-commerce website Sneakers Company. \nFeatures include: \n- navbar transitioning to collapsible sidebar \n- selectible photo gallery \n- cart with CRUD functionality \n- live cart quantity bubble',
+        description: 'A responsive product page web design for a fictional e-commerce website Sneakers Company. \nFeatures include: \n- navbar transitioning to collapsible sidebar \n- selectible photo gallery \n- cart with CRUD functionality \n- live cart quantity bubble',
         sourceCode: 'https://github.com/kabocha23/Ecommerce-Product-Page',
         liveDemo: 'https://kabocha23.github.io/Ecommerce-Product-Page'
     },
     {
-        id: 3,
+        id: 4,
         title: 'Insure',
         headline: 'Insurance Company Landing Page',
         caption: 'React',
@@ -40,27 +50,27 @@ const projectsData = [
         liveDemo: 'https://kabocha23.github.io/Insure-Landing-Page'
     },
     {
-        id: 4,
+        id: 5,
         title: 'Room',
         headline: 'Furniture Store Homepage',
         caption: 'React',
         image: 'room-preview',
-        description: 'A responsive web/mobile design homepage for a fictional furniture company. \nFeatures include: \n- navbar transitioning to hidden menu (sliding in from top) \n- slideshow gallery component',
+        description: 'A responsive web design homepage for a fictional furniture company. \nFeatures include: \n- navbar transitioning to hidden menu (sliding in from top) \n- slideshow gallery component',
         sourceCode: 'https://github.com/kabocha23/Room-Homepage',
         liveDemo: 'https://kabocha23.github.io/Room-Homepage'
     },
     {
-        id: 5,
+        id: 6,
         title: 'Sunnyside Agency',
         headline: 'Graphic Design Company Homepage',
         caption: 'React',
         image: 'sunnyside-preview',
-        description: 'A responsive front end design for a fictional creative agency',
+        description: 'A responsive web design for a fictional creative agency',
         sourceCode: 'https://github.com/kabocha23/Sunnyside-Agency-Site',
         liveDemo: ''
     },
     {
-        id: 6,
+        id: 7,
         title: 'Notes!',
         headline: 'Note keeping app',
         caption: 'React, Firebase Realtime Database',
@@ -70,7 +80,7 @@ const projectsData = [
         liveDemo: ''
     },
     {
-        id: 7,
+        id: 8,
         title: 'Learn in a Flash',
         headline: 'Flashcard App',
         caption: 'React, Firebase Realtime Database',
@@ -80,7 +90,7 @@ const projectsData = [
         liveDemo: 'https://reactflashcards-fb1d7.firebaseapp.com/'
     },
     {
-        id: 8,
+        id: 9,
         title: 'Weather You Like It or Not',
         headline: 'A Weather Lookup App',
         caption: 'React, Firebase Realtime Database',

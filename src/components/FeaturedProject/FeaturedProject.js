2c63f9b4184422a91dc10404b0d7ca947fc1c5b8
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-03 12:47:45 -0800

    moved FeaturedProject component to it's own folder

---
+++ src/components/FeaturedProject/FeaturedProject.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './FeaturedProject.css'
+
+
+const FeaturedProject = React.forwardRef(({ pdId, image, title, headline, caption, description, sourceCode, liveDemo }, ref) => {
+    
+    return (
+        <div className='project-ref-detect' ref={ref}>
+            <div className={pdId % 2 === 0 ? 'project-container isNotVisibleLeft': 'project-container isNotVisibleRight'}  id={`id-${pdId}`}>
+
+                <div className='project-details'>
+                    <div className='project-verbiage'>
+                        <h1>{title}</h1>
+                        <h3>{headline}</h3>
+                        <h4>{caption}</h4>
+                        <div className='project-description'>
+                            {description.split('\n').map(str => <p key={`${str}`}>{str}</p>)}
+                        </div>
+            
+                        <button><a href={`${sourceCode}`} target='blank'>GIT REPO</a></button>
+                        { liveDemo 
+                            ? <button><a href={`${liveDemo}`} target='blank'>LIVE DEMO</a></button>     
+                            : ''
+                        }        
+                    </div>                
+                </div>
+                <div className='project-preview'>
+                    <img src={require(`../../Static/img/${image}.png`)}></img>                
+                </div>
+            </div>            
+        </div>
+
+
+    )
+})
+
+export default FeaturedProject;   

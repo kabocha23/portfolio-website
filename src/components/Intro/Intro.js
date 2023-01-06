f9ce66aa227297769cbff32a17bf549267e2d1f0
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-06 13:02:45 -0800

    changed font style and bg colors

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import './Intro.css';
-
+import React from "react";
+import "./Intro.css";
 
 const Intro = () => {
-
-        return(
-            <div className='intro-container'>
-                <h1 className='intro-name'>Jason Kobuchi</h1>
-                <h3 className='intro-title'>Frontend Developer & Data Analyst</h3>  
-            </div>
-        )
-
-}
+  return (
+    <div className="intro-container">
+      <h1 className="intro-name">Jason Kobuchi</h1>
+      <h3 className="intro-title">Frontend Developer & Data Analyst</h3>
+    </div>
+  );
+};
 
 export default Intro;

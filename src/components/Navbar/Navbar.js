577aecdfba85ea92a2260b09d283ef6315e4c605
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-09 11:52:05 -0800

    changed some divs to semantic html tags

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,65 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
-const Navbar = ({ isNavExpanded, setIsNavExpanded, navBgColor, setNavBgColor, handleHomeClick, handleAboutClick, handleProjectsClick, handleContactClick }) => {
+const Navbar = ({
+  isNavExpanded,
+  setIsNavExpanded,
+  navBgColor,
+  setNavBgColor,
+  handleHomeClick,
+  handleAboutClick,
+  handleProjectsClick,
+  handleContactClick,
+}) => {
+  const toggleIsNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
 
-    const toggleIsNav = () => {
-      setIsNavExpanded(!isNavExpanded)
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 65) {
+      setNavBgColor(true);
+    } else {
+      setNavBgColor(false);
     }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
 
-    const changeNavbarColor = () =>{
-        if(window.scrollY >= 65){
-            setNavBgColor(true);
-        }
-        else{
-            setNavBgColor(false);
-        }
-    }
-    window.addEventListener('scroll', changeNavbarColor);
-  
-    return (
-        <div className={navBgColor ? 'nav-container colorBg' : 'nav-container transpBg'}>
-            <div className='navbar-home'>
-                <a onClick={handleHomeClick}>JK</a>
-            </div>
-            <div className='toggle-button'>
-                <a
-                title='col-nav-btn'
-                onClick={toggleIsNav}
-                >
-                    {isNavExpanded ? 'X' : '☰'}
-                </a>
-            </div>
-            <div className={isNavExpanded ? 'nav-menu expanded' : 'nav-menu'}>
-                <ul className={navBgColor ? 'nav-ul colorBg' : 'nav-ul transpBg'}>
-                    <li><a onClick={handleAboutClick}>About</a></li>
-                    <li><a onClick={handleProjectsClick}>Projects</a></li>
-                    <li><a onClick={handleContactClick}>Contact</a></li>
-                </ul>
-            </div>
-            <div className='nav-menu-fs'>
-                <div className='nav-menu-fs-sub'>
-                    <a onClick={handleAboutClick}>About</a>
-                    <a onClick={handleProjectsClick}>Projects</a>
-                    <a onClick={handleContactClick}>Contact</a>
-                </div>                
-            </div>
+  return (
+    <nav
+      className={
+        navBgColor ? "nav-container colorBg" : "nav-container transpBg"
+      }
+    >
+      <div className="navbar-home">
+        <a onClick={handleHomeClick}>JK</a>
+      </div>
+      <div className="toggle-button">
+        <a title="col-nav-btn" onClick={toggleIsNav}>
+          {isNavExpanded ? "X" : "☰"}
+        </a>
+      </div>
+      <div className={isNavExpanded ? "nav-menu expanded" : "nav-menu"}>
+        <ul className={navBgColor ? "nav-ul colorBg" : "nav-ul transpBg"}>
+          <li>
+            <a onClick={handleAboutClick}>About</a>
+          </li>
+          <li>
+            <a onClick={handleProjectsClick}>Projects</a>
+          </li>
+          <li>
+            <a onClick={handleContactClick}>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="nav-menu-fs">
+        <div className="nav-menu-fs-sub">
+          <a onClick={handleAboutClick}>About</a>
+          <a onClick={handleProjectsClick}>Projects</a>
+          <a onClick={handleContactClick}>Contact</a>
         </div>
-    )
-
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
